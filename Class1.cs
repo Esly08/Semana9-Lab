3a5e9702ea1768a931cbdd862469a2700d95c596
Esly_08 A <125147470+Esly08@users.noreply.github.com>
2023-05-07 09:19:18 -0600

    Actividad semana 9
    El ejercicio muestra el diseño de un forms donde se incluyen varias operaciones.

---
+++ Class1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1308723_AjsivinacEslySem9
+{
+    public class Sumatoria
+    {
+        public void Suma()
+        {
+            int N;
+            double resultado = 0;
+           
+            Console.WriteLine("Porfavor ingrese un número entero positivo: ");
+            N = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= N; i++)
+            {
+                resultado = resultado + (1 / i);
+                Console.WriteLine(resultado);
+            }
+
+        }
+    }
+
+    public class Factorial
+    {
+        public Factorial()
+        {
+            int N2;
+            int factorial = 1;
+            Console.WriteLine("Por favor ingrese un número entero positivo para cálcular su factorial");
+            N2 = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= N2; i++)
+            {
+                factorial = factorial * i;
+            }
+
+            Console.WriteLine("El factorial de " + N2 + " es " + factorial);
+            Console.ReadKey();
+        }
+    }
+
+    public class TablasMultiplicar
+    {
+        public TablasMultiplicar()
+        {
+
+        }
+    }
+
+    public class NumPerfecto
+    {
+        public NumPerfecto()
+        {
+
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,68 @@
+namespace _1308723_AjsivinacEslySem9
+{
+    public partial class Form1 : Form
+    {
+        int _num1 = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+            tabControl1.Visible = true;
+            
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void label2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            Sumatoria resultado = new Sumatoria();
+        }
+    }
+}
